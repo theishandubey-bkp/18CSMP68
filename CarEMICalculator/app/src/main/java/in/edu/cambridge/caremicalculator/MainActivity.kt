773ee673e781590a6bd7a4a8887c76befa6f9291
Ishan Dubey <iamishandubey@gmail.com>
2021-01-22 19:38:41 +0530

    Uploaded Car EMI Calculator Application

---
+++ CarEMICalculator/app/src/main/java/in/edu/cambridge/caremicalculator/MainActivity.kt
@@ -0,0 +1,11 @@
+package `in`.edu.cambridge.caremicalculator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
