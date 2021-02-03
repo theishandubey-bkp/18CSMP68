5659c1dcf2a6f23612a1fc2e29cfa8ee4fd4aefa
Ishan Dubey <iamishandubey@users.noreply.github.com>
2021-02-03 09:38:19 -0800

    Updated Wallpaper Application.

--- Wallpaper/app/src/main/java/in/edu/cambridge/wallpaper/MainActivity.kt
+++ Wallpaper/app/src/main/java/in/edu/cambridge/wallpaper/MainActivity.kt
@@ -3,13 +3,16 @@ package `in`.edu.cambridge.wallpaper
 import android.app.WallpaperManager
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
+import android.os.Handler
 import android.widget.Button
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
+
+    var myWallpaperList = arrayOf(R.drawable.wallpaper1,R.drawable.wallpaper2,R.drawable.wallpaper3,R.drawable.wallpaper4,R.drawable.wallpaper5)
     private lateinit var changeWallpaper:Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,10 +22,14 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun setWallpaper() {
-        val bitmap: Bitmap =
-            BitmapFactory.decodeResource(resources, R.drawable.wallpaper)
-        val wallpaperManager = WallpaperManager.getInstance(baseContext)
-        wallpaperManager.setBitmap(bitmap)
-        Toast.makeText(this, "Wallpaper set!", Toast.LENGTH_SHORT).show()
+        Toast.makeText(this, "Setting Wallpaper please wait.", Toast.LENGTH_SHORT).show()
+        Handler().postDelayed({
+            for(i in myWallpaperList) {
+                val bitmap: Bitmap = BitmapFactory.decodeResource(resources, i)
+                val wallpaperManager = WallpaperManager.getInstance(baseContext)
+                wallpaperManager.setBitmap(bitmap)
+
+            }
+        }, 2000)
     }
 }
